5b0fcecf75fa1b16a93e50c339e07e1dd6155e87
Ham80234 <Ham80234@gmail.com>
2022-03-01 00:40:19 -0600

    init commit

--- Questionnaire_fillin.js
+++ Questionnaire_fillin.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.4.3
 // @description  Autofill the Watchman Implant Questionnaire
-// @author       Adam Meyers
+// @author       Adam Meyers & Andrew Hamlett
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js
 // @require      https://raw.githubusercontent.com/uzairfarooq/arrive/master/minified/arrive.min.js
 // @match        https://www.tampermonkey.net/index.php?version=4.13&ext=dhdg&updated=true
@@ -52,33 +52,54 @@
     function buildFloatingMenu() {
         let floatingMenu = document.createElement("DIV");
         floatingMenu.setAttribute("id", "floatingMenu");
-        floatingMenu.setAttribute("style", "top: " + submitButtonTop + "px; margin: auto; width: 90%; left: 5%; border: 3px solid #73AD21; padding: 70px; background-color:grey; position:absolute; visibility: visible;display: flex;flex-flow: column; row-gap: 10px; align-items: center;");
+        floatingMenu.setAttribute("style", "top: " + submitButtonTop + "px; margin: auto; width: 90%; left: 5%; border: 3px solid black; padding: 70px; background-color:#2A2D34; position:absolute; visibility: visible;display: flex;flex-flow: column; row-gap: 10px; align-items: center;");
 
 
         let qualifiedBtn = document.createElement("BUTTON");
         qualifiedBtn.setAttribute('content', 'Autofill Qualified Form');
         qualifiedBtn.textContent = "Autofill Qualified Form"
-        qualifiedBtn.setAttribute("style", "background: lightgreen; font-family: sans-serif; padding: 10px; border-radius: 10px; width:40%");
+        qualifiedBtn.setAttribute("style", "background: #3D5A80; font-family: sans-serif; padding: 10px; border-radius: 10px; width:40%");
         qualifiedBtn.onclick = fillFormQualified;
 
         let unqualifiedBtn = document.createElement("BUTTON");
         unqualifiedBtn.setAttribute('content', 'Autofill Unqualified Form');
         unqualifiedBtn.textContent = "Autofill Unqualified Form"
-        unqualifiedBtn.setAttribute("style", "background: yellow; font-family: sans-serif; padding: 10px; border-radius: 10px; width:40%");
+        unqualifiedBtn.setAttribute("style", "background: #E0FBFC; font-family: sans-serif; padding: 10px; border-radius: 10px; width:40%");
         unqualifiedBtn.onclick = fillFormUnqualified;
+
+        let qualifiedheadless = document.createElement("BUTTON");
+        qualifiedheadless.setAttribute('content', 'qualified headless');
+        qualifiedheadless.textContent = "Qualified Headless"
+        qualifiedheadless.setAttribute("style", "background: #96BDC6; font-family: sans-serif; padding: 10px; border-radius: 10px; width:40%");
+        qualifiedheadless.onclick = qualifiedHeadless;
+
+        let qualifiedPhoneHeadless = document.createElement("BUTTON");
+        qualifiedPhoneHeadless.setAttribute('content', 'qualified No Phone headless');
+        qualifiedPhoneHeadless.textContent = "Qualified Headless (No Phone)"
+        qualifiedPhoneHeadless.setAttribute("style", "background: #96BDC6; font-family: sans-serif; padding: 10px; border-radius: 10px; width:40%");
+        qualifiedPhoneHeadless.onclick = qualifiedNoPhoneHeadless;
+
+        let unqualifiedHeadless = document.createElement("BUTTON");
+        unqualifiedHeadless.setAttribute('content', 'unqualified  headless');
+        unqualifiedHeadless.textContent = "Unqualified Headless "
+        unqualifiedHeadless.setAttribute("style", "background: #96BDC6; font-family: sans-serif; padding: 10px; border-radius: 10px; width:40%");
+        unqualifiedHeadless.onclick = unqualifiedHeadlessfunc;
 
         let cancelBtn = document.createElement("BUTTON");
         cancelBtn.setAttribute('content', 'CANCEL Autofill');
         cancelBtn.textContent = "CANCEL Autofill"
-        cancelBtn.setAttribute("style", "background: lightpink; font-family: sans-serif; padding: 10px; border-radius: 10px; width:40%");
+        cancelBtn.setAttribute("style", "background: #F05D5E; font-family: sans-serif; padding: 10px; border-radius: 10px; width:40%");
         cancelBtn.onclick = cancelAutoFill;
-        
+
         let versionSpan = document.createElement("span");
         versionSpan.textContent = version;
         versionSpan.setAttribute("style", "position:absolute;top:10px;left:10px;");
 
         floatingMenu.appendChild(qualifiedBtn);
         floatingMenu.appendChild(unqualifiedBtn);
+        floatingMenu.appendChild(qualifiedheadless);
+        floatingMenu.appendChild(qualifiedPhoneHeadless);
+        floatingMenu.appendChild(unqualifiedHeadless);
         floatingMenu.appendChild(cancelBtn);
         floatingMenu.appendChild(versionSpan);
         $(document.body).append(floatingMenu);
@@ -92,8 +113,50 @@
         try {
             Arrive.unbindAllArrive();
         } catch { }
-        
+
         document.getElementById("floatingMenu").style.visibility = "hidden";
+    }
+
+
+    function qualifiedHeadless(){
+       let req_body = generateRequestBody()
+       let url = window.location.origin
+        fetch(`${url}/api/patient`, {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(req_body)
+        }).then(response => response.json())
+          .then(data => {
+            window.location = `${url}/results/${data.patientId}`
+        })
+    }
+
+    function qualifiedNoPhoneHeadless(){
+       let req_body = generateRequestBody()
+       req_body.phone = ""
+       let url = window.location.origin
+        fetch(`${url}/api/patient`, {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(req_body)
+        }).then(response => response.json())
+          .then(data => {
+            window.location = `${url}/results/${data.patientId}`
+        })
+    }
+
+    function unqualifiedHeadlessfunc(){
+       let req_body = generateRequestBody()
+       req_body.surveyResponse = "{\"1001\":[{\"parent_option_id\":10011}],\"1002\":[{\"parent_option_id\":10022}],\"1003\":[{\"parent_option_id\":10031}],\"1004\":[{\"parent_option_id\":10041}],\"1005\":[{\"parent_option_id\":10051}],\"1006\":[{\"parent_option_id\":10061}]}"
+       let url = window.location.origin
+        fetch(`${url}/api/patient`, {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(req_body)
+        }).then(response => response.json())
+          .then(data => {
+            window.location = `${url}/unqualified/${data.patientId}`
+        })
     }
 
     function convertIntToString(num) {
@@ -170,6 +233,22 @@
         }
         return pn;
     }
+
+    function generateRequestBody(){
+        return {
+            "firstName": "firstTest",
+            "lastName": "lastTestbgegbbeadbiag",
+            "phone": `+1 (${Math.floor(Math.random() * 799 + 200)}) ${Math.floor(Math.random() * 899 + 200)}-${Math.floor(Math.random() * 8999 + 1000)}`,
+            "email": `${new Date().valueOf()}@test.com`,
+            "zipCode": "90210",
+            "emailValidationRequired": true,
+            "emailRequired": true,
+            "update": null,
+            "dateOfBirth": "11/11/1911",
+            "surveyResponse": "{\"1001\":[{\"parent_option_id\":10011}],\"1002\":[{\"parent_option_id\":10021}],\"1003\":[{\"parent_option_id\":10031}],\"1004\":[{\"parent_option_id\":10041},{\"parent_option_id\":10042},{\"parent_option_id\":10043}],\"1005\":[{\"parent_option_id\":10051},{\"parent_option_id\":10052},{\"parent_option_id\":10053}],\"1006\":[{\"parent_option_id\":10061},{\"parent_option_id\":10062},{\"parent_option_id\":10063}]}",
+            "surveyId": 1000
+        }
+    };
 
     function initSurvey1000() {
         //  2nd question 'prescribed blood thinners?'
@@ -323,7 +402,7 @@
                 $("button[type=submit]").click();
             }, wait);
         });
-            
+
         // Q7
         $(document).arrive("#question_content_14007", function () {
             $("#question_content_14007 label[for='option_140071']").click();
@@ -341,3 +420,4 @@
         });
     }
 })();
+
